f256d90ba51473554e9dce4573415566529664cd
mohammad javad <chenarbeny2005@gmail.com>
2025-06-16 14:54:20 +0330

    image

--- MJP_realmwar/src/main/java/mjp/utils/ResourceLoader.java
+++ MJP_realmwar/src/main/java/mjp/utils/ResourceLoader.java
@@ -14,39 +14,39 @@ public class ResourceLoader {
         ImageIcon img;
         Image scaledImg;
         try {
-            img = new ImageIcon("/home/sopoyan/Desktop/Realm-War/resources/Barrack.jpg");
+            img = new ImageIcon(getClass().getResource("/Barrack.jpg"));
             scaledImg = img.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             imageIcons[1] = new ImageIcon(scaledImg);
 
-            img = new ImageIcon("/home/sopoyan/Desktop/Realm-War/resources/Farm.jpg");
+            img = new ImageIcon(getClass().getResource("/Farm.jpg"));
             scaledImg = img.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             imageIcons[2] = new ImageIcon(scaledImg);
 
-            img = new ImageIcon("/home/sopoyan/Desktop/Realm-War/resources/Forest.jpg");
+            img = new ImageIcon(getClass().getResource("/Forest.jpg"));
             scaledImg = img.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             imageIcons[3] = new ImageIcon(scaledImg);
 
-            img = new ImageIcon("/home/sopoyan/Desktop/Realm-War/resources/Knight.jpg");
+            img = new ImageIcon(getClass().getResource("/Knight.jpg"));
             scaledImg = img.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             imageIcons[4] = new ImageIcon(scaledImg);
 
-            img = new ImageIcon("/home/sopoyan/Desktop/Realm-War/resources/Peasant.jpg");
+            img = new ImageIcon(getClass().getResource("/Peasant.jpg"));
             scaledImg = img.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             imageIcons[5] = new ImageIcon(scaledImg);
 
-            img = new ImageIcon("/home/sopoyan/Desktop/Realm-War/resources/Spearman.jpg");
+            img = new ImageIcon(getClass().getResource("/Spearman.jpg"));
             scaledImg = img.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             imageIcons[6] = new ImageIcon(scaledImg);
 
-            img = new ImageIcon("/home/sopoyan/Desktop/Realm-War/resources/Swordman.jpg");
+            img = new ImageIcon(getClass().getResource("/Swordman.jpg"));
             scaledImg = img.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             imageIcons[7] = new ImageIcon(scaledImg);
 
-            img = new ImageIcon("/home/sopoyan/Desktop/Realm-War/resources/Tower.jpg");
+            img = new ImageIcon(getClass().getResource("/Tower.jpg"));
             scaledImg = img.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             imageIcons[8] = new ImageIcon(scaledImg);
 
-            img = new ImageIcon("/home/sopoyan/Desktop/Realm-War/resources/Townhall.jpg");
+            img = new ImageIcon(getClass().getResource("/Townhall.jpg"));
             scaledImg = img.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
             imageIcons[9] = new ImageIcon(scaledImg);
         } catch (Exception e) {
