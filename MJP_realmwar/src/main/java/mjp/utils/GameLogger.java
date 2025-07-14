5a202560b3da8c08ef22cbbc57f1e54409ac7336
pooyancoder <poyan.moh1384@gmail.com>
2025-07-15 00:29:11 +0330

    I think is complete

--- MJP_realmwar/src/main/java/mjp/utils/GameLogger.java
+++ MJP_realmwar/src/main/java/mjp/utils/GameLogger.java
@@ -1,7 +1,6 @@
 package mjp.utils;
 
 import com.google.gson.Gson;
-import com.sun.source.tree.TryTree;
 import mjp.controllers.GameController;
 import mjp.models.Kingdom;
 import mjp.models.Player;
@@ -12,6 +11,7 @@ import java.sql.*;
 import java.util.ArrayList;
 
 //       DEMO       DEMO       DEMO       DEMO       DEMO       DEMO       DEMO
+
 public class GameLogger {
     private static final String URL = "jdbc:postgresql://localhost:5432/realm_war"; // must check
     private static final String OWNER = "postgres";
@@ -66,19 +66,28 @@ public class GameLogger {
 
     public void setArrays() {
         for (Player p : Player.getPlayers()) {
-            this.players.add(gson.toJson(p));
+            if (!players.contains(gson.toJson(p))) {
+                this.players.add(gson.toJson(p));
+            }
         }
 
+
         for (Kingdom k : Kingdom.getKingdoms()) {
-            this.kingdoms.add(gson.toJson(k));
+            if (!kingdoms.contains(gson.toJson(k))) {
+                this.kingdoms.add(gson.toJson(k));
+            }
         }
 
         for (Structure s : Structure.getStructures()) {
-            this.structures.add(gson.toJson(s));
+            if (!structures.contains(gson.toJson(s))) {
+                this.structures.add(gson.toJson(s));
+            }
         }
 
         for (Unit u : Unit.getUnits()) {
-            this.units.add(gson.toJson(u));
+            if (!units.contains(gson.toJson(u))) {
+                this.units.add(gson.toJson(u));
+            }
         }
     }
 
@@ -98,7 +107,6 @@ public class GameLogger {
             }
         }
     }
-
 
     public void query() {
 //        System.out.println("(exit button action listener)gameLogger>>save>>query>>complete sql code");
@@ -181,6 +189,94 @@ public class GameLogger {
         kingdoms = new ArrayList<>();
         units = new ArrayList<>();
         structures = new ArrayList<>();
+    }
+
+    public void readFromDateBase() {
+        readFromPlayer();
+        readFromKingdom();
+        readFromStructure();
+        readFromUnit();
+        gameController.fillAttributes();
+    }
+
+    public void readFromPlayer() {
+        String sql = "SELECT json text FROM player";
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, OWNER, PASSWORD);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                players.add(resultSet.getString("json text"));
+            }
+        } catch (SQLException e) {
+            System.out.println("read from player failed");
+        }
+
+        for (String s : players) {
+            Player.getPlayers().add(gson.fromJson(s, Player.class));
+        }
+    }
+
+    public void readFromKingdom() {
+        String sql = "SELECT json text FROM kingdom";
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, OWNER, PASSWORD);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                kingdoms.add(resultSet.getString("json text"));
+            }
+        } catch (SQLException e) {
+            System.out.println("read from kingdom failed");
+        }
+
+        for (String s : kingdoms) {
+            Kingdom.getKingdoms().add(gson.fromJson(s, Kingdom.class));
+        }
+    }
+
+    public void readFromStructure() {
+        String sql = "SELECT json text FROM structure";
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, OWNER, PASSWORD);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                structures.add(resultSet.getString("json text"));
+            }
+        } catch (SQLException e) {
+            System.out.println("read from structure failed");
+        }
+
+        for (String s : structures) {
+            Structure.getStructures().add(gson.fromJson(s, Structure.class));
+        }
+    }
+
+    public void readFromUnit() {
+        String sql = "SELECT json text FROM unit";
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, OWNER, PASSWORD);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                units.add(resultSet.getString("json text"));
+            }
+        } catch (SQLException e) {
+            System.out.println("read from unit failed");
+        }
+
+        for (String s : units) {
+            Unit.getUnits().add(gson.fromJson(s, Unit.class));
+        }
     }
 
     public void createTables() {  // SHOULD CHANGE
